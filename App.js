29fa4a8d5fe18616d5bf93e23fe4400316941d83
Mo Ghasemzadeh <mo.zadeh@berkeley.edu>
2020-06-29 14:48:55 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
-  Picker,
   View,
   StyleSheet,
 } from 'react-native';
+import { Picker } from '@react-native-community/picker';
 
 const styles = StyleSheet.create({
   container: {
